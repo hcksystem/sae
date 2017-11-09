f65195979b6c53778ec2ba2a35c98bccc314f36f
Luis Salazar <pastorchessmaster@gmail.com>
2017-11-08 20:14:36 -0500

    Prueba de git en vscode

--- server/persistencia/index.php
+++ server/persistencia/index.php
@@ -3,4 +3,5 @@ include_once("./AdministradorBaseDatos.php");
 
 $conexion = new AdministradorBaseDatos("local");
 $respuesta = $conexion->ejecutarConsulta("SELECT * from Genero;");
+
 echo json_encode($respuesta);
