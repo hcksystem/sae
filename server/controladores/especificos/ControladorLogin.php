5adfe7c98eb33808711521b0b715962ddf4e30ed
Luis Salazar <pastorchessmaster@gmail.com>
2017-11-23 22:03:16 -0500

    listo login google

--- server/controladores/especificos/ControladorLogin.php
+++ server/controladores/especificos/ControladorLogin.php
@@ -5,14 +5,18 @@ class ControladorLogin extends ControladorBase
 {
    function login(String $email, String $clave)
    { 
-      $in=imap_open("{pop.gmail.com:995/pop3/ssl}",$email, $clave);
-      if($in==false){
+       $in=imap_open("{pop.gmail.com:995/pop3/ssl}",$email, $clave);
+       if($in==false){
           return false;
-      }
-      imap_close($in);
-      $sql = "SELECT Persona.id, Cuenta.idRol FROM Persona INNER JOIN Cuenta ON Cuenta.idPersona = Persona.id WHERE Persona.correoElectronico = ?;";
-      $parametros = array($email);
-      $respuesta = $this->conexion->ejecutarConsulta($sql,$parametros);
-      return $respuesta;
+       }
+       imap_close($in);
+       $sql = "SELECT Persona.id, Cuenta.idRol FROM Persona INNER JOIN Cuenta ON Cuenta.idPersona = Persona.id WHERE Persona.correoElectronico = ?;";
+       $parametros = array($email);
+       $respuesta = $this->conexion->ejecutarConsulta($sql,$parametros);
+       if(is_null($respuesta[0])||$respuesta[0]==0){
+          return false;
+       }else{
+          return $respuesta[0];
+       }
    }
 }
