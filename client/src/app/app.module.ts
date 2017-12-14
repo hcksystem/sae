0d3e6fadacbc974009eab857843ecb58aaefbd90
Luis Salazar <pastorchessmaster@gmail.com>
2017-12-13 21:13:59 -0500

    Compilable

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,46 +1,53 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { Http, HttpModule } from '@angular/http';
+import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthGuard } from './shared';
+import { FormsModule } from '@angular/forms';
 import { ToastModule } from 'ng2-toastr/ng2-toastr';
-import { BsComponentModule } from 'app/layout/bs-component/bs-component.module';
-import { PersonaService } from 'app/CRUD/persona/persona.service';
+import { BsComponentModule } from './layout/bs-component/bs-component.module';
+import { PersonaService } from './CRUD/persona/persona.service';
+import { Http, HttpModule } from '@angular/http';
+import { ToastsManager } from 'ng2-toastr/ng2-toastr';
+import { ToastOptions } from 'ng2-toastr/src/toast-options';
+import { ToastContainer } from 'ng2-toastr/src/toast-container.component';
 
 // AoT requires an exported function for factories
-export function HttpLoaderFactory(http: Http) {
+export function createTranslateLoader(http: HttpClient) {
     // for development
-    // return new TranslateHttpLoader(http, '/start-angular/SB-Admin-BS4-Angular-4/master/dist/assets/i18n/', '.json');
-    return new TranslateHttpLoader(http, '/assets/i18n/', '.json');
+    // return new TranslateHttpLoader(http, '/start-angular/SB-Admin-BS4-Angular-5/master/dist/assets/i18n/', '.json');
+    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
 @NgModule({
-    declarations: [
-        AppComponent
-    ],
     imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
-        FormsModule,
         HttpModule,
         AppRoutingModule,
+        FormsModule,
         BsComponentModule,
+        CommonModule,
+        BrowserModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
         TranslateModule.forRoot({
             loader: {
                 provide: TranslateLoader,
-                useFactory: HttpLoaderFactory,
-                deps: [Http]
+                useFactory: createTranslateLoader,
+                deps: [HttpClient]
             }
         }),
-        ToastModule.forRoot()
+        AppRoutingModule
     ],
-    providers: [AuthGuard, PersonaService],
+    declarations: [AppComponent],
+    providers: [AuthGuard,
+        PersonaService,
+        ToastsManager,
+        ToastOptions],
     bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule {}
