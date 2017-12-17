520444afa5d9027489ce2dbe4b13b896448a4f43
Luis Salazar <pastorchessmaster@gmail.com>
2017-12-16 23:27:45 -0500

    Router Matriculacion Funcionando

--- client/src/app/shared/components/sidebar/sidebar.component.ts
+++ client/src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,13 +1,54 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { LoginResult } from 'app/entidades/especifico/Login-Result';
 @Component({
     selector: 'app-sidebar',
     templateUrl: './sidebar.component.html',
     styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
     isActive = false;
     showMenu = '';
+    estudiante: Boolean;
+    secretariaAcademica: Boolean;
+    tutor: Boolean;
+    sec: Boolean;
+    rol: number;
+    rolMatriculacion: Boolean;
+    constructor() {}
+
+    ngOnInit() {
+        const logedResult = JSON.parse(localStorage.getItem('logedResult')) as LoginResult;
+        this.rol = logedResult.idRol;
+        this.estudiante = false;
+        this.tutor = false;
+        this.secretariaAcademica = false;
+        this.rolMatriculacion = false;
+        switch (this.rol) {
+            case 1:
+                this.rolMatriculacion = false;
+                break;
+            case 2:
+                this.estudiante = true;
+                this.rolMatriculacion = true;
+                break;
+            case 3:
+                this.rolMatriculacion = false;
+                break;
+            case 4:
+                this.tutor = true;
+                this.rolMatriculacion = true;
+                break;
+            case 5:
+                this.rolMatriculacion = true;
+                this.secretariaAcademica = true;
+                break;
+            case 6:
+                this.rolMatriculacion = true;
+                this.estudiante = true;
+                break;
+        }
+    }
+
     eventCalled() {
         this.isActive = !this.isActive;
     }
