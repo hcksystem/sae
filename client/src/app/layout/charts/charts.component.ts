859f69dce926348f0dac760db7b9202ceab22ce9
Luis Salazar <pastorchessmaster@gmail.com>
2017-12-29 12:32:17 -0500

    jugando con charts

--- client/src/app/layout/charts/charts.component.ts
+++ client/src/app/layout/charts/charts.component.ts
@@ -13,12 +13,12 @@ export class ChartsComponent implements OnInit {
         scaleShowVerticalLines: false,
         responsive: true
     };
-    public barChartLabels: string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+    public barChartLabels: string[] = ['Hombres', 'Mujeres', '2008', '2009', '2010', '2011', '2012'];
     public barChartType: string = 'bar';
     public barChartLegend: boolean = true;
 
     public barChartData: any[] = [
-        { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+        { data: [654, 456, 245, 1.5, 1.7, 2, 4], label: 'Series A' },
         { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
     ];
     // Doughnut
