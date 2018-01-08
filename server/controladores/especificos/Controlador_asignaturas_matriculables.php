ace50d3f12cd7237edb38d7b1bb8bf0f5c45b58d
pasthortown <pastorchessmaster@gmail.com>
2018-01-08 15:36:27 -0500

    Asignaturas matriculables

---
+++ server/controladores/especificos/Controlador_asignaturas_matriculables.php
@@ -0,0 +1,18 @@
+<?php
+include_once('../controladores/Controlador_Base.php');
+class Controlador_asignaturas_matriculables extends Controlador_Base
+{
+    function consultar($args)
+    { 
+        $identificacion = $args["identificacion"];
+        // CIFRAR LA CLAVE
+        $sql = "SELECT Asignatura.* FROM Asignatura INNER JOIN AsignaturaCupo ON AsignaturaCupo.idAsignatura = Asignatura.id INNER JOIN Cupo ON AsignaturaCupo.idCupo = Cupo.id INNER JOIN PeriodoLectivo ON PeriodoLectivo.id = Cupo.idPeriodoLectivo INNER JOIN Persona ON Cupo.idPersona = Persona.id WHERE Persona.identificacion = ?;";
+        $parametros = array($identificacion);
+        $respuesta = $this->conexion->ejecutarConsulta($sql,$parametros);
+        if(is_null($respuesta[0])||$respuesta[0]==0){
+           return false;
+        }else{
+            return $respuesta;
+        }   
+   }
+}
