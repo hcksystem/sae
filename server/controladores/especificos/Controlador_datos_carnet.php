08d26d748a5efe5ab1aa62df7375371751237c54
pasthortown <pastorchessmaster@gmail.com>
2018-01-23 19:58:21 -0500

    datos carnet listo

--- server/controladores/especificos/Controlador_datos_carnet.php
+++ server/controladores/especificos/Controlador_datos_carnet.php
@@ -5,11 +5,11 @@ class Controlador_datos_carnet extends Controlador_Base
    function consultar($args)
    { 
         $identificacion = $args["identificacion"];
-        $sql = "SELECT Persona.id, Persona.identificacion, UPPER(CONCAT(Persona.apellido1,' ' , Persona.apellido2,' ' , Persona.nombre1,' ' , Persona.nombre2)) as 'nombreCompleto' FROM Persona WHERE Persona.identificacion = ?;";
+        $sql = "SELECT Persona.id as idPersona, Persona.identificacion, UPPER(CONCAT(Persona.apellido1,' ' , Persona.apellido2)) as 'apellidos', UPPER(CONCAT(Persona.nombre1,' ' , Persona.nombre2)) as 'nombres' FROM Persona WHERE Persona.identificacion = ?;";
         $parametros = array();
         array_push($parametros, $identificacion);
         $respuesta = $this->conexion->ejecutarConsulta($sql,$parametros)[0];
-        $idPersona = $respuesta['id'];
+        $idPersona = $respuesta['idPersona'];
         $sql = "SELECT Estudiante.id FROM Estudiante WHERE idPersona = ?;";
         $parametros = array();
         array_push($parametros, $idPersona);
