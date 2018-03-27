942c729266534450a45e89e2ecf907b7fc2c8eb6
albertandoca <albertandoca@gmail.com>
2018-03-27 11:08:20 -0500

    datos aspirantes

--- server/controladores/especificos/Controlador_asistencias_estudiante.php
+++ server/controladores/especificos/Controlador_asistencias_estudiante.php
@@ -23,15 +23,28 @@ class Controlador_asistencias_estudiante extends Controlador_Base
         $respuesta = $this->conexion->ejecutarConsulta($sql, $parametros);  
         
         if($respuesta[0] == 0){
-            
-            $sql = "SELECT MatriculaAsignatura.id AS idMatriculaAsignatura, MatriculaAsignatura.idAsignatura, MatriculaAsignatura.idParalelo,
+            $sql = "CREATE TEMPORARY TABLE tabla_tmp SELECT Persona.id AS idPersona, SUM(Asistencia.horas) AS totalHoras FROM Asistencia
+                INNER JOIN MatriculaAsignatura ON MatriculaAsignatura.id = Asistencia.idMatriculaAsignatura
+                INNER JOIN Matricula ON Matricula.id = MatriculaAsignatura.idMatricula
+                INNER JOIN Persona ON Persona.id = Matricula.idPersona
+                WHERE MatriculaAsignatura.idAsignatura = ? AND  MatriculaAsignatura.idParalelo = ? AND Matricula.idPeriodoLectivo = ?
+                GROUP BY Persona.id
+                ORDER BY apellido1, Persona.Apellido2, Persona.Nombre1, Persona.nombre2;";
+            $parametros = array($idAsignatura, $idParalelo, $idPeriodoLectivo);
+            $respuesta = $this->conexion->ejecutarConsulta($sql, $parametros);
+
+            $sql = "SELECT Persona.id AS idPersona, MatriculaAsignatura.id AS idMatriculaAsignatura, MatriculaAsignatura.idAsignatura, MatriculaAsignatura.idParalelo,
                 CONCAT(Persona.apellido1, \" \", Persona.apellido2, \" \", Persona.nombre1, \" \", Persona.nombre2) AS nombre,
-                ? AS fecha, ? AS horas FROM MatriculaAsignatura
+                ? AS fecha, ? AS horas, tabla_tmp.totalHoras FROM MatriculaAsignatura
                 INNER JOIN (Matricula INNER JOIN Persona ON Matricula.idPersona = Persona.id) ON Matricula.id = MatriculaAsignatura.idMatricula
+                INNER JOIN tabla_tmp ON tabla_tmp.idPersona = Persona.id
                 WHERE MatriculaAsignatura.idAsignatura = ? AND  MatriculaAsignatura.idParalelo = ? AND Matricula.idPeriodoLectivo = ?
                 ORDER BY nombre;";
             $parametros = array($fecha, $horas, $idAsignatura, $idParalelo, $idPeriodoLectivo);
-            $respuesta = $this->conexion->ejecutarConsulta($sql, $parametros);   
+            $respuesta = $this->conexion->ejecutarConsulta($sql, $parametros);
+            
+            
+            
         }
         return $respuesta;
    }
